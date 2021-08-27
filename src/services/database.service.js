8260daf4e7b5ff6b814afc4387e201ccff5809a5
yeshvanthsgit <yeshvanthbk@gmail.com>
2021-08-27 09:38:20 +0000

    jenkins commit

--- src/services/database.service.js
+++ src/services/database.service.js
@@ -1,4 +1,10 @@
 const MockDatabaseService = require("./class.mock.database.service");
+const FirestoreDatabaseService = require("./class.firestore.database.service");
+let db;
+if(!!process.env.TESTING) {
+    db = new MockDatabaseService();
+} else {
+    db = new FirestoreDatabaseService();
+}
 
-const db = new MockDatabaseService();
 module.exports = db;
